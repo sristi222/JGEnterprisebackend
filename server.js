ef2bf9af2fa031b561fb9b3694de8b2e1638a5d5
Sristi Shrestha <132660312+sristi222@users.noreply.github.com>
2025-06-09 01:13:12 +0545

    Update server.js

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.use(
       "http://localhost:5174",
       "https://jg-enterprisefrontend-xxce.vercel.app/", // ✅ Allow deployed frontend
       "https://jg-enterprisefrontend-xxce-a084fyrod.vercel.app" // ✅ Add this!
+      "https://jg-enterprisefrontend-qxv5gn7wq.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
